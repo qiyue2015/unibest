d269ac4b2e5889361ec2179ff3d231aec5e10c6c
白开水浇花 <fengqiyue@gmail.com>
2025-04-13 18:23:00 +0800

    feat: 优化用户登录流程

--- src/store/user.ts
+++ src/store/user.ts
@@ -40,25 +40,30 @@ export const useUserStore = defineStore(
     // 获取用户信息
     const getUserInfo = async () => {
       try {
-        const login = () => {
+        const loginFlow = () => {
           wx.login({
-            success: function ({ code }) {
-              authSessionOpenid(code)
-                .then(({ data }) => {
-                  setSessionid(data.sessionid)
-                  setUserInfo(data.userinfo)
-                })
-                .catch(() => {
-                  clearUserInfo()
-                  login() // 调用登录
-                })
+            success: async function ({ code }) {
+              try {
+                let { data } = await authSessionOpenid(code)
+
+                // 由于首次登录时 userinfo 为空，为了简化后续业务逻辑，这里重新获取一次
+                // 参考文档: https://wiki.w7.com/document/35/1026
+                if (!data?.userinfo) {
+                  data = (await authSessionOpenid(code)).data
+                }
+
+                setSessionid(data.sessionid)
+                setUserInfo(data.userinfo)
+              } catch {
+                clearUserInfo()
+              }
             },
           })
         }
         if (sessionid.value) {
           checkSessionid() // 调用检查 sessionid 是否过期
         } else {
-          login() // 调用登录
+          loginFlow() // 调用登录
         }
       } catch (error) {
         clearUserInfo()
