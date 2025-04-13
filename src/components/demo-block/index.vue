23a1941d8a3a92cfa193b16cf3b3b4f3929e51ae
白开水浇花 <fengqiyue@gmail.com>
2025-04-14 05:16:12 +0800

    feat: 新增demo-block组件

---
+++ src/components/demo-block/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <view class="demo-block">
+    <view class="px-4 py-2 text-sm">{{ title }}</view>
+    <view class="demo-container">
+      <slot />
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  title?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: '',
+})
+</script>
+
+<style lang="scss" scoped>
+.demo-block {
+  color: #666;
+
+  &:not(:first-child) {
+    margin-top: 15px;
+  }
+
+  &:not(:last-child) {
+    margin-bottom: 15px;
+  }
+}
+</style>
