84b2a289b634ef6c4b32a0476dbf3391f586faa5
白开水浇花 <fengqiyue@gmail.com>
2025-05-25 20:45:26 +0800

    fix: 更新API请求路径，添加缺失的查询参数

--- src/api/viewer.ts
+++ src/api/viewer.ts
@@ -7,17 +7,17 @@ export interface IViewer {
 }
 
 export const getViewerList = () => {
-  return http.get<IViewer[]>('/app/index.php?c=entry&m=basketball&do=viewer&op=list')
+  return http.get<IViewer[]>('/app/index.php?a=wxapp&c=entry&m=basketball&do=viewer&op=list')
 }
 
 export const saveViewer = (data: IViewer) => {
-  return http.post<IViewer>('/app/index.php?c=entry&m=basketball&do=viewer&op=save', data)
+  return http.post<IViewer>('/app/index.php?a=wxapp&c=entry&m=basketball&do=viewer&op=save', data)
 }
 
 export const deleteViewer = (id: string) => {
-  return http.post<IViewer>('/app/index.php?c=entry&m=basketball&do=viewer&op=delete', { id })
+  return http.post<IViewer>('/app/index.php?a=wxapp&c=entry&m=basketball&do=viewer&op=delete', { id })
 }
 
 export const updateViewer = (data: IViewer) => {
-  return http.post<IViewer>('/app/index.php?c=entry&m=basketball&do=viewer&op=update', data)
+  return http.post<IViewer>('/app/index.php?a=wxapp&c=entry&m=basketball&do=viewer&op=update', data)
 }
