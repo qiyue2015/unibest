a64110034490b1d6e3025feb88d754eed3aa321b
白开水浇花 <fengqiyue@gmail.com>
2025-05-30 23:51:04 +0800

    style(layouts): 更新默认主题颜色变量

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -10,8 +10,8 @@
 import type { ConfigProviderThemeVars } from 'wot-design-uni'
 
 const themeVars: ConfigProviderThemeVars = {
-  // colorTheme: 'red',
-  // buttonPrimaryBgColor: '#07c160',
+  colorTheme: '#07c160',
+  buttonPrimaryBgColor: '#00B386',
   // buttonPrimaryColor: '#07c160',
 }
 </script>
