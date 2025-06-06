142fc3b2000432bfdb1583c7a965418707dd5960
白开水浇花 <fengqiyue@gmail.com>
2025-06-07 01:45:11 +0800

    refactor(api/app): 重构 API 调用，统一使用 http 方法并添加 noAuth 选项

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,23 +1,33 @@
 import { http } from '@/utils/http'
 
 export const getModuleSetting = () => {
-  return http.get('/app/index.php?a=wxapp&c=entry&m=qiyue_workorder&do=setting&op=display')
+  return http({
+    url: '/app/index.php?a=wxapp&c=entry&m=qiyue_workorder&do=setting&op=display',
+    noAuth: true,
+  })
 }
 
 export const getActivityDetail = (id: number) => {
-  return http.get('/app/index.php?a=wxapp&c=entry&m=basketball&do=activity&op=detail', {
-    id,
+  return http({
+    url: '/app/index.php?a=wxapp&c=entry&m=basketball&do=activity&op=detail',
+    data: { id },
+    noAuth: true,
   })
 }
 
 // 赛事赛程例表
 export const getScheduleList = () => {
-  return http.get<any>('/app/index.php?a=wxapp&c=entry&m=basketball&do=schedule&op=list')
+  return http({
+    url: '/app/index.php?a=wxapp&c=entry&m=basketball&do=schedule&op=list',
+    noAuth: true,
+  })
 }
 
 // 赛事赛程详情
 export const getScheduleDetail = (id: number) => {
-  return http.get<any>('/app/index.php?a=wxapp&c=entry&m=basketball&do=schedule&op=detail', {
-    id,
+  return http({
+    url: '/app/index.php?a=wxapp&c=entry&m=basketball&do=schedule&op=detail',
+    data: { id },
+    noAuth: true,
   })
 }
