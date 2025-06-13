bc5f9f1566958e1d2e32b8c57d5dc28963537bbb
白开水浇花 <fengqiyue@gmail.com>
2025-06-13 15:41:11 +0800

    feat(globalShareMixin): 添加全局分享混合功能

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { createSSRApp } from 'vue'
 import App from './App.vue'
 import { prototypeInterceptor, requestInterceptor, routeInterceptor } from './interceptors'
 import store from './store'
+import globalShareMixin from './utils/globalShareMixin'
 
 export function createApp() {
   const app = createSSRApp(App)
@@ -14,6 +15,7 @@ export function createApp() {
   app.use(requestInterceptor)
   app.use(prototypeInterceptor)
   app.use(VueQueryPlugin)
+  app.mixin(globalShareMixin)
 
   return {
     app,
---
+++ src/utils/globalShareMixin.ts
@@ -0,0 +1,15 @@
+export default {
+  onShareAppMessage() {
+    return {
+      title: '2025 凉山州“火洛杯”禁毒防艾男子篮球联赛',
+      path: '/pages/index/index',
+      imageUrl: 'http://mp-img1.wifixc.com/images/94/2025/06/z3bYNndwNiAzytNC0zoA3VN3AlWtnC.jpg',
+    }
+  },
+  onShareTimeline() {
+    return {
+      title: '2025 凉山州“火洛杯”禁毒防艾男子篮球联赛',
+      imageUrl: 'http://mp-img1.wifixc.com/images/94/2025/06/z3bYNndwNiAzytNC0zoA3VN3AlWtnC.jpg',
+    }
+  },
+}
